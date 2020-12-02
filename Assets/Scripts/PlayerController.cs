af3a3673a56e33bb2f9e29a5feedca1a133aacaa
James Khen <jkhenman@my.centennialcollege.ca>
2020-12-01 23:19:47 -0500

    Unity Files added

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horiz, vert;
+        horiz = Input.GetAxis("Horizontal");
+        vert = Input.GetAxis("Jump");
+        Debug.Log(horiz);
+
+        Vector2 newVelocity = new Vector2(horiz, vert);
+
+        GetComponent<Rigidbody2D>().velocity = newVelocity * 5;
+
+    }
+}
